ca883881ffe57139ef74bc2f024a74b6367bbcfd
choyoung1119 <choyoung1119@gmail.com>
2025-01-12 17:34:30 +0900

    Update dependencies and clean up imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider, CssBaseline, AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { ThemeProvider, CssBaseline } from '@mui/material';
 import theme from './theme';
 import Home from './pages/Home';
 import Admin from './pages/Admin';
@@ -10,16 +10,6 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Newsletter Service
-            </Typography>
-            <Button color="inherit" href="/admin">
-              Admin
-            </Button>
-          </Toolbar>
-        </AppBar>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/admin" element={<Admin />} />
